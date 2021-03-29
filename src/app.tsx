bfa56a1f757fddc54d9e7ae87096acca087f0b2e
Josh Searle <joshryansearle@gmail.com>
2021-03-29 20:33:13 +1100

    Add routes

--- src/app.tsx
+++ src/app.tsx
@@ -1,17 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Router, Switch, Route } from 'react-router';
 import { createBrowserHistory } from 'history';
 
 import Home from './components/home';
+import About from './components/about';
+import Skills from './components/skills';
+import Projects from './components/projects';
+import Navbar from './components/navbar';
+
+type RouteObj = {
+  path: string;
+  name: string;
+  element: JSX.Element;
+};
+
+const x = <Home />;
 
 const App: React.FC = () => {
+  const routes: RouteObj[] = [
+    { path: '/', name: 'Home', element: <Home /> },
+    { path: '/about', name: 'About', element: <About /> },
+    { path: '/projects', name: 'Projects', element: <Projects /> },
+    { path: '/skills', name: 'Skills', element: <Skills /> },
+  ];
+
+  const routeElements = routes.map((route) => (
+    <Route path={route.path}>{route.element}</Route>
+  ));
   return (
     <Router history={createBrowserHistory()}>
-      <Switch>
-        <Route path='/'>
-          <Home />
-        </Route>
-      </Switch>
+      <Navbar />
+      <Switch>{routeElements}</Switch>
     </Router>
   );
 };
