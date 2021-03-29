7dda738ce2049d6b3b5dc41b65513736b965f808
Josh Searle <joshryansearle@gmail.com>
2021-03-29 20:20:38 +1100

    Added routing

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import { Router, Switch, Route } from 'react-router';
+import { createBrowserHistory } from 'history';
+
+import Home from './components/home';
 
 const App: React.FC = () => {
-    return <h1>app</h1>
-}
+  return (
+    <Router history={createBrowserHistory()}>
+      <Switch>
+        <Route path='/'>
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  );
+};
 
 export default App;
