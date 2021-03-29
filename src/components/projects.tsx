a212653ac37f0d978347dc21fcaa18b2fdde27b7
Josh Searle <joshryansearle@gmail.com>
2021-03-29 20:07:48 +1100

    Formatting

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Projects: React.FC = () => {
-    return <h1>Projects</h1>
-}
+  return <h1>Projects</h1>;
+};
 
 export default Projects;
--- src/components/skills.tsx
+++ src/components/skills.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Skills: React.FC = () => {
-    return <h1>Skills</h1>
-}
+  return <h1>Skills</h1>;
+};
 
 export default Skills;
