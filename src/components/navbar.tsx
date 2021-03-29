024a9d7699d71dddcf167c0d6c2fb603edb86e10
Josh Searle <joshryansearle@gmail.com>
2021-03-29 20:55:46 +1100

    Add navbar component

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const Navbar: React.FC = () => {
-  return <h1>Navbar</h1>;
+import { RouteName } from '../types';
+
+interface NavbarProps {
+  links: RouteName[];
+}
+
+const Navbar: React.FC<NavbarProps> = ({ links }) => {
+  const linkElements = links.map((link) => (
+    <Link to={link.path}>{link.name}</Link>
+  ));
+  return <nav>{linkElements}</nav>;
 };
 
 export default Navbar;
