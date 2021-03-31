97f3a564edf6f6aecc61457ec1d4726d21debd26
Josh Searle <joshryansearle@gmail.com>
2021-03-31 20:23:34 +1100

    Custom hook for clicking

---
+++ src/hooks/use_clicked.ts
@@ -0,0 +1,14 @@
+import { useState, useEffect } from 'react';
+
+const useClicked = (): [boolean, () => void] => {
+  const [clicked, setClicked] = useState(false);
+  const click = () => setClicked(true);
+  useEffect(() => {
+    if (!clicked) return;
+    setTimeout(() => setClicked(false), 30);
+  }, [clicked])
+
+  return [clicked, click];
+}
+
+export default useClicked;
