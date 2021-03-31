4832874047a0c10ca503425196beec8399507df2
Josh Searle <joshryansearle@gmail.com>
2021-03-31 20:24:01 +1100

    Update navbar to use custom hook

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -3,29 +3,17 @@ import { Link } from 'react-router-dom';
 
 import { RouteName } from '../types';
 import classes from '../css/classes';
+import useClicked from '../hooks/use_clicked';
 
 interface NavbarProps {
   links: RouteName[];
 }
 
 const Navbar: React.FC<NavbarProps> = ({ links }) => {
-  // If a click occured in the last life of the navbar, it will be stored here
-  const [navbarClicked, setNavbarClicked] = useState(false);
-
-  // If navbarClicked changed from previous life of navbar, run this
-  useEffect(() => {
-    // If it was 'unclicked', do nothing
-    if (!navbarClicked) return;
-    // If it was 'clicked', set it unclicked
-    // Need to time out, to make sure the page renders once without the css class,
-    // otherwise, sometimes the animation does not run
-    // Users cannot notice 30ms
-    const MILLISECONDS_BEFORE_UPDATE = 30;
-    setTimeout(() => setNavbarClicked(false), MILLISECONDS_BEFORE_UPDATE);
-  }, [navbarClicked]);
+  const [clicked, click] = useClicked();
 
   // Set the css class list based on navbarClicked state
-  const classList = navbarClicked
+  const classList = clicked
     ? classes.navbar
     : `${classes.navbar} ${classes.navbarAnimation}`;
 
@@ -35,7 +23,7 @@ const Navbar: React.FC<NavbarProps> = ({ links }) => {
   ));
 
   return (
-    <nav onClick={() => setNavbarClicked(true)} className={classList}>
+    <nav onClick={click} className={classList}>
       {linkElements}
     </nav>
   );
