dbb2a7058c8213eded7aabb374d14553b3a1a0ba
Josh Searle <joshryansearle@gmail.com>
2021-04-01 18:35:20 +1100

    Add project tiles to project page

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
+import ProjectTile from './project_tile';
+
+import projects from '../data/projects';
+
 const Projects: React.FC = () => {
-  return <h1>Projects</h1>;
+  const projectElements = projects.map((p) => <ProjectTile project={p} />);
+  return <div>{projectElements}</div>;
 };
 
 export default Projects;
