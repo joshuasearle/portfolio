ef46509a5a6216c823c6603a98520fcdb901e2b1
Josh Searle <joshryansearle@gmail.com>
2021-04-01 20:21:00 +1100

    Add title to project page

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -3,10 +3,16 @@ import React from 'react';
 import ProjectTile from './project_tile';
 
 import projects from '../data/projects';
+import classes from '../css/classes';
 
 const Projects: React.FC = () => {
   const projectElements = projects.map((p) => <ProjectTile project={p} />);
-  return <div>{projectElements}</div>;
+  return (
+    <div>
+      <h1 className={classes.title}>My Projects</h1>
+      <div className={classes.projects}>{projectElements}</div>
+    </div>
+  );
 };
 
 export default Projects;
