4e922ec803e9c205b92c13c169deb76d3bcae280
Josh Searle <joshryansearle@gmail.com>
2021-04-01 18:06:52 +1100

    Add technology enum

--- src/types.ts
+++ src/types.ts
@@ -8,3 +8,31 @@ export type RouteName = {
   path: string;
   name: string;
 }
+
+export enum Technology {
+  JavaScript,
+  React,
+  Nodejs,
+  TypeScript,
+  Webpack,
+  Websockets,
+  Java,
+  Python,
+  HTML,
+  CSS,
+  Bootstrap,
+  Angular, 
+  Chartjs,
+  Redux,
+  Mongodb,
+  GoogleCloudFunctions,
+  Express
+}
+
+export type Project = {
+  name: string;
+  description: string;
+  link?: string;
+  source: string;
+  technologies: Technology[];
+}
